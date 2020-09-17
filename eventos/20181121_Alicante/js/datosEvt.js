ff07c4b98baa5188f468e65766a23c8e9cac32cd
Camada51 <37265990+Camada51@users.noreply.github.com>
2020-09-17 11:23:59 +0200

    Add files via upload

---
+++ eventos/20181121_Alicante/js/datosEvt.js
@@ -0,0 +1,16 @@
+﻿//datos del evento de Alicante del 21/11/2018
+
+nombreEvt = "20181121_Alicante";
+directorioEvt = "20181121_Alicante";
+tituloEvt = "Reunión en Alicante";
+fechaEvt = new Date("2018", "10", "21"); //año,mes,día (al mes hay que restarle 1, enero =0)
+poblacionEvt = "Alicante";
+localEvt = "Restaurante Club Montemar,Carrer Virgili 25";
+
+//Los asistentes aparecerán en el orden que sigue.
+
+
+asistentesEvt = "Eduardo Albarracín;José Luís Alonso;José Alós;Vicente José Anguiz;Juan Bautista Aragonés;Vicente Bertomeu;Juan Antonio Carreño;Rafael Chacón;Rafael Cremades;Bento Da Cruz;Antonino De Mora;Julio Delgado;José Escolano;Juan Miguel Feliu;Emilio Ferreirós;Luís García;Facundo Giménez;Juan Francisco Giner;José Antonio Gisbert;Antonio Jesús González;Manuel Antonio Guillén;Amando Guillén;Carlos Hornillos;Rafael Ladrón;Francisco Llorca;Jose Miguel Lloret;Enrique de Madaria;Ignacio Madrona;Juan Andrés Maltés;Javier Luis Massa;Miguel Pastor;Francisco Ángel Penalva;Ramón Peyró;Luís Prats;Juan Bautista Puigcerver;Roberto Pablo Reig;Enrique Reolid;Pedro Revenga;Ricardo Reyes;Salvador Ros;Martín Salinas;Bartolomé Sánchez;Ramón Joaquín Sirvent;Luís Soler;Carlos Suay;Héctor Terol;Luís Torres;Clemente Ubeda;José María Villaescusa";
+fotosEvt = "clubMontemar.jpg,P1030378.JPG,2018-11-21 at 19.40.54.jpeg,2018-11-21 at 19.40.55.jpeg,2018-11-21 at 19.40.55_01.jpeg,2018-11-21 at 19.40.55_02.jpeg,2018-11-21 at 19.40.55_03.jpeg,2018-11-21 at 19.40.56.jpeg";
+
+menuEvt = "Buen recuerdo, pero sin detalles, debe ser la edad...";
