04e707f7cc98baad8e33c228fe65c994d0c71f4b
Camada51 <37265990+Camada51@users.noreply.github.com>
2020-09-17 11:22:57 +0200

    Add files via upload

---
+++ eventos/20180921_Albacete/js/datosEvt.js
@@ -0,0 +1,20 @@
+﻿//datos del evento de Albacete del 21/09/2018
+
+nombreEvt = "20180921_Albacete";
+directorioEvt = "20180921_Albacete";
+tituloEvt = "Reunión en Albacete";
+fechaEvt = new Date("2018", "8", "21"); //año,mes,día (al mes hay que restarle 1, enero =0)
+poblacionEvt = "Albacete";
+localEvt = "Restaurante El Callejón, c/ Guzmán el Bueno 18";
+asistentesEvt = "Eduardo Albarracín;Facundo Giménez Mena;Carlos Hornillos Blasco;Rafael Ladrón de Guevara;Francisco Llorca;Antonino de Mora Montes;Juan Diego Pérez Pérez;José María Pertusa;Ramón Peyró;Bartolomé Sánchez;Luis Torres;Clemente Úbeda";
+fotosEvt = "ElCallejon_Google.jpg,WhatsApp Image 2018-09-21 at 20.32.43.jpeg,Chapulines_2018-09-16 at 19.44.59.jpeg,WhatsApp Image 2018-09-21 at 13.17.44.jpeg,WhatsApp Image 2018-09-21 at 13.21.14.jpeg,WhatsApp Image 2018-09-21 at 20.32.33(1).jpeg,WhatsApp Image 2018-09-21 at 20.32.33.jpeg,WhatsApp Image 2018-09-21 at 20.32.34.jpeg,WhatsApp Image 2018-09-21 at 20.32.43(1).jpeg,WhatsApp Image 2018-09-21 at 20.32.44.jpeg";
+
+menuEvt = "José María nos obsequió con los exóticos Chapulines.<br/>" +
+    "Rafa nos regalo su ya tradicional perdiz en escabeche.<br/>" +
+    "<br/>El menú:<br/><br/>" +
+    "&nbsp;&nbsp;&nbsp; Jamón ibérico de bellota<br/>" +
+    "&nbsp;&nbsp;&nbsp; Ensalada de ventresca<br/>" +
+    "&nbsp;&nbsp;&nbsp; Brocheta de zamburiña con espaquetti de sepia<br/>" +
+    "&nbsp;&nbsp;&nbsp; Tapa de pulpo sobre patata<br/>" +
+    "&nbsp;&nbsp;&nbsp; Taco de bacalao al horno<br/>" +
+    "&nbsp;&nbsp;&nbsp; Degustación estofado de rabo<br/>";
