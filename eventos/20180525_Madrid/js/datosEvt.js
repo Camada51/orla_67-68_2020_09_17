c7b82166ef33fbafa8e6935dabeff838fc2743b5
Camada51 <37265990+Camada51@users.noreply.github.com>
2020-09-17 11:21:48 +0200

    Add files via upload

---
+++ eventos/20180525_Madrid/js/datosEvt.js
@@ -0,0 +1,14 @@
+﻿//datos del evento de Madrid del 25/05/2018
+
+nombreEvt = "20180525_Madrid";
+directorioEvt = "20180525_Madrid";
+tituloEvt = "Reunión en Madrid";
+fechaEvt = new Date("2018", "4", "25"); //año,mes,día (al mes hay que restarle 1, enero =0)
+poblacionEvt = "Madrid";
+localEvt = "Casa Baranda, c/ Colón 11";
+
+asistentesEvt = "Bartolomé Sánchez Bernat;Carlos Hornillos;Clemente Úbeda;Luis Torres;Eduardo Albarracín;Eduardo Toba;Francisco Llorca;José María Pertusa;José Miguel Villaescusa;Rafael Alenda;Rafael Ladrón de Guevara;Ramón Peyró";
+
+fotosEvt = "CasaBaranda_Bing.jpg,2018-05-25_14.45.13.jpeg,2018-05-25_16.36.06.jpeg,2018-05-25_16.36.24.jpeg,2018-05-25_16.36.48.jpeg,2018-05-25_16.37.03.jpeg,2018-05-25_17.33.22.jpeg,2018-05-25_17.33.40.jpeg,2018-05-25_17.34.08.jpeg,2018-05-25_20.19.59.jpeg";
+
+menuEvt = "Fué original y variado. Hubo \"fabada con almejas\" y otras delicias.<br/><br/>Los que se acuerden que lo digan para completar.<br/><br/><br/>Rafael obsequió a los presentes con unas perdices escabechadas<br/> de factura propia que fueron muy celebradas y agradecidas.";
